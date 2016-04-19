4706ed10a9beebbe1cbfc63bd38f29d65bb4f06c
ahmarkhanpathan <ahmarkhan_p@yahoo.com>
2016-04-19 19:52:32 +0530

    JavaSession1_assignment4

---
+++ src/package4/Assignment4.java
@@ -0,0 +1,22 @@
+package package4;
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=5, y= 10;
+		
+		int a= x+y*2;
+		int b= x-y+2;
+		int c= (x+y)*2;
+		int d= y % x;
+        
+		System.out.println("Where x=5 and y=10");
+		System.out.println("x+y*2="+(a));
+		System.out.println("x-y+2="+(b));
+		System.out.println("(x+y)*2="+(c));
+		System.out.println("y%x="+(d));
+	}
+
+}
